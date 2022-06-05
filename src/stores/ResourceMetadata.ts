55c6f842183392c162354cca39f60ecdd190b802
Samuel-planchon <71815717+Samuel-planchon@users.noreply.github.com>
2022-06-05 18:22:51 +0100

    v finale
    end internship

--- src/stores/ResourceMetadata.ts
+++ src/stores/ResourceMetadata.ts
@@ -1,5 +1,5 @@
 /*
-Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Samuel PLANCHON William BERNHARD
 SPDX-License-Identifier: Apache-2.0
 */
 export default interface ResourceMetadata {
